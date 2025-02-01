0f2d977db4220b08d9891090a70d16e201d6e01a
daibeemachine <ols00559@morris.umn.edu>
2025-02-01 11:45:09 -0600

    Modified the setupEach test function
    In TodoControllerSpec, the setupEach function now adds owners that
    correspond with the values in a todo.

---
+++ server/src/test/java/umm3601/todos/TodoControllerSpec.java
@@ -0,0 +1,201 @@
+package umm3601.todos;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.json.JavalinJackson;
+
+/**
+ * Tests the logic of the TodoController
+ *
+ * @throws IOException
+ */
+// The tests here include a ton of "magic numbers" (numeric constants).
+// It wasn't clear to me that giving all of them owners would actually
+// help things. The fact that it wasn't obvious what to call some
+// of them says a lot. Maybe what this ultimately means is that
+// these tests can/should be restructured so the constants (there are
+// also a lot of "magic strings" that Checkstyle doesn't actually
+// flag as a problem) make more sense.
+@SuppressWarnings({ "MagicNumber" })
+class TodoControllerSpec {
+
+  // An instance of the controller we're testing that is prepared in
+  // `setupEach()`, and then exercised in the various tests below.
+  private TodoController TodoController;
+
+  // A Mongo object ID that is initialized in `setupEach()` and used
+  // in a few of the tests. It isn't used all that often, though,
+  // which suggests that maybe we should extract the tests that
+  // care about it into their own spec file?
+  private ObjectId JimmysId;
+
+  // The client and database that will be used
+  // for all the tests in this spec file.
+  private static MongoClient mongoClient;
+  private static MongoDatabase db;
+
+  // Used to translate between JSON and POJOs.
+  private static JavalinJackson javalinJackson = new JavalinJackson();
+
+  @Mock
+  private Context ctx;
+
+  @Captor
+  private ArgumentCaptor<ArrayList<Todo>> TodoArrayListCaptor;
+
+  @Captor
+  private ArgumentCaptor<Todo> TodoCaptor;
+
+  @Captor
+  private ArgumentCaptor<Map<String, String>> mapCaptor;
+
+  /**
+   * Sets up (the connection to the) DB once; that connection and DB will
+   * then be (re)used for all the tests, and closed in the `teardown()`
+   * method. It's somewhat expensive to establish a connection to the
+   * database, and there are usually limits to how many connections
+   * a database will support at once. Limiting ourselves to a single
+   * connection that will be shared across all the tests in this spec
+   * file helps both speed things up and reduce the load on the DB
+   * engine.
+   */
+  @BeforeAll
+  static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(
+        MongoClientSettings.builder()
+            .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+            .build());
+    db = mongoClient.getDatabase("test");
+  }
+
+  @AfterAll
+  static void teardown() {
+    db.drop();
+    mongoClient.close();
+  }
+
+  @BeforeEach
+  void setupEach() throws IOException {
+    // Reset our mock context and argument captor (declared with Mockito
+    // annotations @Mock and @Captor)
+    MockitoAnnotations.openMocks(this);
+
+    // Setup database
+    MongoCollection<Document> TodoDocuments = db.getCollection("Todos");
+    TodoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    testTodos.add(
+        new Document()
+            .append("owner", "Chris")
+            .append("status", false)
+            .append("body", "In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.")
+            .append("category", "software design"));
+    testTodos.add(
+      new Document()
+      .append("owner", "Fry")
+      .append("status", true)
+      .append("body", "Ullamco irure laborum magna dolor non. Anim occaecat adipisicing cillum eu magna in.")
+      .append("category", "homework"));
+    testTodos.add(
+      new Document()
+      .append("owner", "Jill")
+      .append("status", false)
+      .append("body", "this is a potatoman")
+      .append("category", "beat villans"));
+
+
+            // There are two examples of suppressing CheckStyle
+
+
+
+    JimmysId = new ObjectId();
+    Document Jimmy = new Document()
+        .append("_id", JimmysId)
+        .append("owner", "Jimmy")
+        .append("status", false)
+        .append("body", "Jimmy shall climb Mt. Everest and make a delicious pie")
+        .append("category", "jimmy things");
+
+    TodoDocuments.insertMany(testTodos);
+    TodoDocuments.insertOne(Jimmy);
+
+    TodoController = new TodoController(db);
+  }
+
+  @Test
+  void addsRoutes() {
+    Javalin mockServer = mock(Javalin.class);
+    TodoController.addRoutes(mockServer);
+    verify(mockServer, Mockito.atLeast(3)).get(any(), any());
+    verify(mockServer, Mockito.atLeastOnce()).post(any(), any());
+    verify(mockServer, Mockito.atLeastOnce()).delete(any(), any());
+  }
+
+  @Test
+  void canGetAllTodos() throws IOException {
+    // When something asks the (mocked) context for the queryParamMap,
+    // it will return an empty map (since there are no query params in
+    // this case where we want all Todos).
+    when(ctx.queryParamMap()).thenReturn(Collections.emptyMap());
+
+    // Now, go ahead and ask the TodoController to getTodos
+    // (which will, indeed, ask the context for its queryParamMap)
+    TodoController.getUsers(ctx);
+
+    // We are going to capture an argument to a function, and the type of
+    // that argument will be of type ArrayList<Todo> (we said so earlier
+    // using a Mockito annotation like this):
+    // @Captor
+    // private ArgumentCaptor<ArrayList<Todo>> TodoArrayListCaptor;
+    // We only want to declare that captor once and let the annotation
+    // help us accomplish reassignment of the value for the captor
+    // We reset the values of our annotated declarations using the command
+    // `MockitoAnnotations.openMocks(this);` in our @BeforeEach
+
+    // Specifically, we want to pay attention to the ArrayList<Todo> that
+    // is passed as input when ctx.json is called --- what is the argument
+    // that was passed? We capture it and can refer to it later.
+    verify(ctx).json(TodoArrayListCaptor.capture());
+    verify(ctx).status(HttpStatus.OK);
+
+    // Check that the database collection holds the same number of documents
+    // as the size of the captured List<Todo>
+    assertEquals(
+        db.getCollection("Todos").countDocuments(),
+        TodoArrayListCaptor.getValue().size());
+  }
+}
